39766eb7cca96ba872a6b475ff272a18b40f6bde
Manuel Arce <90724931+manuel040192@users.noreply.github.com>
2022-08-27 17:08:46 +0200

    Update 1_accounts.js

--- examples/1_accounts.js
+++ examples/1_accounts.js
@@ -1,1 +1,14 @@
+const { ethers } = require('ethers');
 
+const INFURA_ID = 'abc';
+
+const provider = new ethers.providers.JsonRpcProvider(`https://mainnet.infura.io/v3/${INFURA_ID}`);
+
+const address = '0x73BCEb1Cd57C711feaC4224D062b0F6ff338501e'
+
+async function main() {
+    const balance = await provider.getBalance(address);
+    console.log(`ETH Balance of ${address} --> ${ethers.utils.formatEther(balance)} ETH`);
+}
+
+main()
