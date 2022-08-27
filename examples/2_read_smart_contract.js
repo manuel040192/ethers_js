f78ad28028b759b80e1bc9c36a142e5e6647145c
Manuel Arce <90724931+manuel040192@users.noreply.github.com>
2022-08-27 17:31:55 +0200

    Create 2_read_smart_contract.js

---
+++ examples/2_read_smart_contract.js
@@ -0,0 +1,23 @@
+const { ethers } = require("ethers");
+
+const INFURA_ID = 'abc'
+
+const provider = new ethers.providers.JsonRpcProvider(`https://mainnet.infura.io/v3/${INFURA_ID}`)
+
+const ERC20_ABI = [
+    "function name() view returns (string)",
+    "function symbol() view returns (string)",
+    "function totalSupply() view returns (uint256)",
+    "function balanceOf(address) view returns (uint)",
+];
+
+const address = '0x6B175474E89094C44Da98b954EedeAC495271d0F' // DAI Contract
+
+const contract = new ethers.Contract(address, ERC20_ABI, provider)
+
+async function main() {
+    const name = await contract.name()
+    console.log('name:', name)
+}
+
+main()
