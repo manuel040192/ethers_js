3d5d2661f6c1a8ebc3a36fbac048b990df75cbd6
Manuel Arce <90724931+manuel040192@users.noreply.github.com>
2022-08-27 20:22:59 +0200

    Update 2_read_smart_contract.js

--- examples/2_read_smart_contract.js
+++ examples/2_read_smart_contract.js
@@ -11,13 +11,20 @@ const ERC20_ABI = [
     "function balanceOf(address) view returns (uint)",
 ];
 
-const address = '0x6B175474E89094C44Da98b954EedeAC495271d0F' // DAI Contract
+const address = '0x6B175474E89094C44Da98b954EedeAC495271d0F' // DAI contract
 
 const contract = new ethers.Contract(address, ERC20_ABI, provider)
 
 async function main() {
     const name = await contract.name()
-    console.log('name:', name)
+    const symbol = await contract.symbol()
+    const totalSupply = await contract.totalSupply()
+    const balance = await contract.balanceOf('0x6c6Bc977E13Df9b0de53b251522280BB72383700') // DAI token holder
+    console.log(`Name: ${name}`)
+    console.log(`Symbol: ${symbol}`)
+    console.log(`Total supply: ${totalSupply}`)
+    console.log(`Raw balance: ${balance}`)
+    console.log(`Formatted balance: ${ethers.utils.formatEther(balance)}`)
 }
 
 main()
